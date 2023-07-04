5a9fd2ce038b2214667b17d278ab732a53c74aba
SnowArcher <s.work.viktor@gmail.com>
2023-07-04 18:52:14 +0300

    fix pagination

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Pagination({totalPosts, postsPerPage, setCurrentPage, currentPage}) {
     const pages = [];
     for(let i = 1; i <= Math.ceil(totalPosts/postsPerPage); i++) {
         pages.push(i);
     }
+    useEffect(() => {
+        if (currentPage > Math.ceil(totalPosts/postsPerPage) && currentPage != 1) {
+            setFirst(firstButton - 6); 
+            setLast(lastButton - 6);
+            setCurrentPage(Math.ceil(totalPosts/postsPerPage));
+        }
+    }, [totalPosts])
     const [firstButton, setFirst] = useState(0);
     const [lastButton, setLast] = useState(6);
     return (
